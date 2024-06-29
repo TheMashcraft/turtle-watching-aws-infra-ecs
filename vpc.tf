06a217c19f419ca3449e4ca17043e65abc1b285c
Matt Ashcraft <ashcraft39@gmail.com>
2024-06-29 12:57:02 -0400

    Add count in route table association

--- vpc.tf
+++ vpc.tf
@@ -42,6 +42,7 @@ resource "aws_route_table" "main" {
 }
 
 resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main.id
+  count          = length(var.subnet_cidrs)
+  subnet_id      = aws_subnet.main[count.index].id
   route_table_id = aws_route_table.main.id
 }
