e127d067424e88777edba827481f37ed4b2eafc5
Matt Ashcraft <ashcraft39@gmail.com>
2024-06-29 20:41:45 -0400

    Fix route table id

--- endpoint.tf
+++ endpoint.tf
@@ -20,5 +20,5 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_vpc.main.route_table_id]
+  route_table_ids   = [aws_vpc.main.default_route_table_id]
 }
