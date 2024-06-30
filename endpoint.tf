e36d6988a9bdf9e9553bff5da411f00b814d5f53
Matt Ashcraft <ashcraft39@gmail.com>
2024-06-29 20:28:00 -0400

    Add private endpoints for ECR and S3

---
+++ endpoint.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = aws_subnet.main[*].id
+  security_group_ids = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = aws_subnet.main[*].id
+  security_group_ids = [aws_security_group.vpc_endpoints_sg.id]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_vpc.main.route_table_id]
+}
